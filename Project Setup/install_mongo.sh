5764ac2408b2a82005ca6746cbb00ea0af233bfc
Chanpreet Singh <chanpreets10@gmail.com>
2024-02-02 04:55:28 -0400

    Project setup related files

---
+++ Project Setup/install_mongo.sh
@@ -0,0 +1,10 @@
+# This shell script is for installing MongoDB 4.0.1 in a debian based machine(x86_64)
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt update
+sudo apt install mongodb-org
+sudo apt install mongodb-org=4.0.1 mongodb-org-server=4.0.1 mongodb-org-shell=4.0.1 mongodb-org-mongos=4.0.1 mongodb-org-tools=4.0.1
+sudo systemctl enable mongod
+sudo systemctl start mongod
+sudo service mongod start
+sudo service mongod status
